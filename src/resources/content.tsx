f57f03008da8ec3264f7b0715796dca73398283e
Vedantp2 <vedantpatel.app@gmail.com>
2026-03-03 16:02:48 -0500

    Update content.tsx

--- src/resources/content.tsx
+++ src/resources/content.tsx
@@ -60,7 +60,7 @@ const home: Home = {
   label: "Home",
   title: `${person.name}'s Portfolio`,
   description: `Portfolio website showcasing my work as a ${person.role}`,
-  headline: <>Building bridges between engineering and design</>,
+  headline: <>Engineering Fueled by Curiosity</>,
   featured: {
     display: false,
     title: (
