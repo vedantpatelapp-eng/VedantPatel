98c6408644c48f0f422b13fb1bffb2eaf980e220
Vedantp2 <vedantpatel.app@gmail.com>
2026-03-04 18:16:01 -0500

    hh

--- src/resources/once-ui.config.ts
+++ src/resources/once-ui.config.ts
@@ -14,7 +14,7 @@ import {
 import { home } from "./index";
 
 // IMPORTANT: Replace with your own domain address - it's used for SEO in meta tags and schema
-const baseURL: string = "https://demo.magic-portfolio.com";
+const baseURL: string = "https://thevedantpatel.vercel.app/";
 
 const routes: RoutesConfig = {
   "/": true,
