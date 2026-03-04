75be293194a02c6b841a78573da52c8b2b43d4b6
Vedantp2 <vedantpatel.app@gmail.com>
2026-03-04 17:26:35 -0500

    Update content.tsx

--- src/resources/content.tsx
+++ src/resources/content.tsx
@@ -225,7 +225,7 @@ const about: About = {
 const blog: Blog = {
   path: "/blog",
   label: "Blog",
-  title: "Writing about design and tech...",
+  title: "I build things. I break things. I write about both.",
   description: `Read what ${person.name} has been up to recently`,
   // Create new blog posts by adding a new .mdx file to app/blog/posts
   // All posts will be listed on the /blog route
